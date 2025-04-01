24cd34cb7fed50f112e64d034d1b12e38f3df8a2
kmatos7793 <88529154+kmatos7793@users.noreply.github.com>
2025-04-02 06:33:22 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "1073971";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "1073981";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "1073972";
